237926896c32b620f52479e6e2ca36097562747f
zm <1109282075@qq.com>
2020-06-11 22:54:44 +0800

    合并二叉树

---
+++ 合并二叉树.js
@@ -0,0 +1,14 @@
+var mergeTrees = function(t1, t2) {
+    if(t1 === null){
+        return t2
+    }else if(t2 === null){
+        return t1
+    }else{
+        t1.val = t1.val + t2.val
+        t1.left = mergeTrees(t1.left, t2.left)
+        t1.right = mergeTrees(t1.right, t2.right)
+
+        return t1
+    }
+    
+};
